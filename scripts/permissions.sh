11e70fe014800749c639663ee675853f2b831f06
Isaac Lo <isaaclo123@gmail.com>
2021-01-12 12:59:57 -0800

    chown

--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-chown -R ec2:ec2 /home/ec2-user/tiktak-node
+chown -R ec2-user:ec2-user /home/ec2-user/tiktak-node
 ##Allows any users to write the app folder. Useful if using fs within the app
 # sudo chmod go+w /var/www/TikTak
